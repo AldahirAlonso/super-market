6abbd3e6f2e7c4176bb35a40e49cc202f6c8cad5
Aldahir Alonso <aldahir0603@hotmail.com>
2025-04-29 21:30:08 -0600

    Implementacion de Query Methods

--- src/main/java/com/platzi/market/persistence/ProductoRepository.java
+++ src/main/java/com/platzi/market/persistence/ProductoRepository.java
@@ -4,11 +4,20 @@ import com.platzi.market.persistence.crud.ProductoCrudReporitory;
 import com.platzi.market.persistence.entity.Producto;
 
 import java.util.List;
+import java.util.Optional;
 
 public class ProductoRepository {
     private ProductoCrudReporitory productoCrudReporitory;
 
     public List<Producto> getAll() {
         return (List<Producto>) productoCrudReporitory.findAll();
+    }
+
+    public List<Producto> getByCategoria(int idCategoria) {
+        return productoCrudReporitory.findByIdCategoriaOrderByNombreAsc(idCategoria);
+    }
+
+    public Optional<List<Producto>> getEscasos(int cantidad) {
+        return productoCrudReporitory.findByCantidadStockLessThanAndEstado(cantidad, true);
     }
 }
