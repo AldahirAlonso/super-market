2aa8af00fdf5e727994c031cc68e392586b5edda
Aldahir Alonso <aldahir0603@hotmail.com>
2025-05-23 17:45:12 -0600

    Creando Dominio de Compras

---
+++ src/main/java/com/platzi/market/domain/Purchase.java
@@ -0,0 +1,14 @@
+package com.platzi.market.domain;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+public class Purchase {
+    private int purchaseId;
+    private String clientId;
+    private LocalDateTime date;
+    private String paymentMethod;
+    private String comment;
+    private String state;
+    private List<PurchaseItem> item;
+}
